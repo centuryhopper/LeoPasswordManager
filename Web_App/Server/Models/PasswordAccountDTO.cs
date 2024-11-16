372076d42e50a92cbd8d65fd59dacbb02c22d029
centuryhopper <43508492+centuryhopper@users.noreply.github.com>
2024-11-15 21:05:36 -0500

    update

--- Web_App/Server/Models/PasswordAccountDTO.cs
+++ Web_App/Server/Models/PasswordAccountDTO.cs
@@ -2,10 +2,9 @@ namespace Shared.Models;
 
 using System.Collections.Generic;
 using CsvHelper.Configuration;
-using HandyBlazorComponents.Interfaces;
 
 
-public class PasswordAccountDTO : IHandyGridEntity
+public class PasswordAccountDTO
 {
     public int Id { get; set; }
     public int UserId { get; set; }
@@ -14,79 +13,6 @@ public class PasswordAccountDTO : IHandyGridEntity
     public string Password { get; set; } = string.Empty;
     public DateTime? CreatedAt { get; set; }
     public DateTime? LastUpdatedAt { get; set; }
-
-    public object? DisplayPropertyInGrid(string propertyName)
-    {
-        switch (propertyName)
-        {
-            case nameof(Id):
-                return Id;
-            case nameof(Title):
-                return Title;
-            case nameof(UserId):
-                return UserId;
-            case nameof(Username):
-                return Username;
-            case nameof(Password):
-                return Password;
-            case nameof(CreatedAt):
-                return CreatedAt is null ? DateTime.Now.ToString("yyyy-MM-dd") : CreatedAt.GetValueOrDefault().ToString("yyyy-MM-dd");
-            case nameof(LastUpdatedAt):
-                return LastUpdatedAt is null ? DateTime.Now.ToString("yyyy-MM-dd") : LastUpdatedAt.GetValueOrDefault().ToString("yyyy-MM-dd");
-            default:
-                throw new Exception("Invalid property name");
-        }
-    }
-
-    public int GetPrimaryKey()
-    {
-        return Id;
-    }
-
-    public void ParsePropertiesFromCSV(Dictionary<string, object> properties)
-    {
-        foreach (var property in properties)
-        {
-            // Use reflection to set the property if it exists and is writable
-            var propInfo = this.GetType().GetProperty(property.Key);
-            if (propInfo != null && propInfo.CanWrite)
-            {
-                var propType = propInfo.PropertyType;
-
-                // Check if the property is a List<string> and the value is a delimited string
-                if (typeof(IEnumerable<string>).IsAssignableFrom(propType))
-                {
-                    var stringValue = ((IEnumerable<string>) property.Value).First();
-                    // Convert the delimited string to a List<string>
-                    var listValue = stringValue.Split('+').ToList();
-                    propInfo.SetValue(this, listValue);
-                }
-                else
-                {
-                    // Set the property directly if it's not a List<string> or delimited string
-                    propInfo.SetValue(this, property.Value);
-                }
-            }
-        }
-    }
-
-    public void SetPrimaryKey(int id)
-    {
-        Id = id;
-    }
-
-    public void SetProperties(Dictionary<string, object> properties)
-    {
-        foreach (var property in properties)
-        {
-            // Use reflection to set the property if it exists and is writable
-            var propInfo = this.GetType().GetProperty(property.Key);
-            if (propInfo != null && propInfo.CanWrite)
-            {
-                propInfo.SetValue(this, property.Value);
-            }
-        }
-    }
 
     public override string ToString()
     {
